f8d79fe63889d5b12107f92d3a146347d52512ab
danilofernandes45 <danilofernandes45@gmail.com>
2022-04-11 13:57:48 -0300

    Probabilities updater - Deletion Case

--- LocalSearch_Nargesian.cpp
+++ LocalSearch_Nargesian.cpp
@@ -25,6 +25,7 @@ class Instance
         int total_num_columns; // TOTAL NUMBER OF COLUMNS
         int embedding_dim; // WORD VECTOR EMBEDDING DIMENSION
         Table **tables = NULL; // TABLES FROM DATA LAKE
+        int **map; // MAP ABSOLUTE (IN DL) IN RELATIVE (IN ITS TABLE) COLUMN ID
 };
 
 class State
@@ -32,14 +33,17 @@ class State
     public:
         float *sum_vector; //SUM VECTOR REPRESENTATION OF THE STATE
         int sample_size;  // NUMBER OF VALUES (WORD VECTORS) UNDER ITS DOMAIN
-        int level;        // THE SHORTEST PATH LENGTH FROM THE ROOT
+        int level = -1;        // THE SHORTEST PATH LENGTH FROM THE ROOT
         float *reach_probs; // REACHABILITY PROBABILITIES GIVEN EACH INTERESTING ATTRIBUTE (ONLY FOR NON-LEAF NODES)
         float overall_reach_prob; // OVERALL REACHABILITY PROBABILITY OF THE STATE (ONLY FOR NON-LEAF NODES)
         float discover_prob; // DISCOVER PROBABILITY OF A ATTRIBUTE REPRESENTED BY THE STATE (ONLY FOR LEAF NODES)
         vector<State*> parents;
         vector<State*> children;
-        int table_id = -1;
-        int column_id= -1;
+        int abs_column_id= -1; //ABSOLUTE COLUMN ID (ONLY FOR LEAF NODES)
+        float *similarities_vector; //VECTOR WITH SIMILARITIES BETWEEN THIS STATE AND ALL INTERESTING TOPICS IN DL (ONLY FOR NON-LEAF NODES)
+
+    void update_children_reach_probs(Instance *inst, float gamma);
+
 };
 
 class Cluster
@@ -52,40 +56,108 @@ class Cluster
         int cardinality; //NUMBER OF ELEMENTS CONTAINED IN THIS CLUSTERS
 };
 
-//CHAINED LIST (QUEUE)
-class Node
-{
-    public:
-        State *state = NULL;
-        Node *next = NULL;
-};
-
 class Organization
 {
     public:
         int embedding_dim;
         State *root = NULL;
         float gamma; // HYPERPARAMETER USED IN PROBABILITY ESTIMATION
-
-    void update_all_reach_probs(Instance *inst);
-
+    void compute_all_reach_probs(Instance *inst);
 };
 
-void Organization::update_all_reach_probs(Instance *inst){
+//IMPLEMENTATION CONSIDERING THAT ALL PARENTS OF A STATE ARE IN THE SAME LEVEL
+//UNDER THIS CONSIDERATION, A BREADTH-FIRST SEARCH CAN UPDATE ALL PARENTS BEFORE ITS CHILDREN
+void State::update_children_reach_probs(Instance *inst, float gamma)
+{
+    queue<State*> queue;
+    queue.push(this);
+    State *current;
+    State *parent;
+    float sum_probs, prob;
+
+    while( !queue.empty() ){
+        //GET THE FIRST STATE FROM THE QUEUE
+        current = queue.front();
+        queue.pop();
+
+        //COMPUTE ITS REACHABILITY PROBABILITIES
+        if( current->level != 0 && current->abs_column_id < 0 )
+        {
+            //UPDATE LEVEL OF CURRENT NODE. OBS.: ALL PARENTS ARE IN THE SAME LEVEL
+            current->level = current->parents[0]->level + 1;
+
+            //TEST!
+            printf("Level: %d\nID: %d\n", current->level, current->abs_column_id);
+            printf("Reach Probs\n");
+            
+            current->overall_reach_prob = 0;
+            //FOR EACH INTERESTING TOPIC
+            for (int i = 0; i < inst->total_num_columns; i++)
+            {
+                //FOR EACH PARENT
+                current->reach_probs[i] = 0; 
+                for(int j = 0; j < current->parents.size(); j++)
+                {
+                    parent = current->parents[j];
+                    sum_probs = 0;
+                    //WARNING: THIS CAN BE OPTIMIZED
+                    for(int k = 0; k < parent->children.size(); k++)
+                        sum_probs += exp( gamma * parent->children[k]->similarities_vector[i] / parent->children.size());
+                    
+                    prob = exp( gamma * current->similarities_vector[i] / parent->children.size() ) / sum_probs;
+                    current->reach_probs[i] += prob * parent->reach_probs[i];
+                    //TEST!
+                    printf("%.3f ", current->reach_probs[i]);
+                }
+                current->overall_reach_prob += current->reach_probs[i];
+            }
+            current->overall_reach_prob /= inst->total_num_columns;
+            //TEST!
+            printf("\nOverall reach prob: %.3f\n\n", current->overall_reach_prob);
+        //COMPUTE ITS DISCOVERY PROBABILITY (WHEN IT IS A LEAF)
+        } else if ( current->abs_column_id >= 0 ){
+
+            //UPDATE LEVEL OF CURRENT NODE. OBS.: ALL PARENTS ARE IN THE SAME LEVEL
+            current->level = current->parents[0]->level + 1;
+
+            //TEST!
+            printf("Level: %d\nID: %d\n", current->level, current->abs_column_id);
+            printf("Reach Probs\n");
+
+            //FOR EACH PARENT
+            current->discover_prob = 0;
+            for(int j = 0; j < current->parents.size(); j++)
+            {
+                parent = current->parents[j];
+                sum_probs = 0;
+                for(int k = 0; k < parent->children.size(); k++){
+                    sum_probs += exp( gamma * parent->children[k]->similarities_vector[current->abs_column_id] / parent->children.size());
+                    printf("%d %.3f\n", parent->abs_column_id, parent->children[k]->similarities_vector[current->abs_column_id]);
+                }
+                prob = exp( gamma * current->similarities_vector[current->abs_column_id] / parent->children.size() ) / sum_probs;
+                current->discover_prob += prob * parent->reach_probs[current->abs_column_id];
+            }
+            printf("Discover probability: %.3f\n\n", current->discover_prob);
+        }
+        
+
+        //ADD ITS CHILDREN TO THE QUEUE
+        for(int i=0; i < current->children.size(); i++ )
+            queue.push(current->children[i]);
+        
+    }
+
+}
+
+void Organization::compute_all_reach_probs(Instance *inst){
 
     this->root->level = 0;
     this->root->overall_reach_prob = 1;
     this->root->reach_probs = new float(inst->total_num_columns);
     for (int i = 0; i < inst->total_num_columns; i++)
         this->root->reach_probs[i] = 1.0;
-    
-    Node *queue = new Node;
-    queue->state = this->root;
 
-    while( queue != NULL )
-    {
-
-    }
+    this->root->update_children_reach_probs(inst, this->gamma);
 }
 
 float cossine_similarity(float *vector_1, float *vector_2, int dim){
@@ -125,6 +197,7 @@ Organization generate_basic_organization(Instance * inst, float gamma)
     State *state;
     float aux;
     float ratio;
+    int count = 0;
 
     //Leaf nodes (columns representation) creation
     for (int i = 0; i < inst->num_tables; i++)
@@ -134,8 +207,8 @@ Organization generate_basic_organization(Instance * inst, float gamma)
         for (int j = 0; j < inst->tables[i]->ncols; j++)
         {
             state = new State;
-            state->table_id = i;
-            state->column_id = j;
+            state->abs_column_id = count;
+            count++;
             state->parents.push_back( org.root );
             state->sum_vector = inst->tables[i]->sum_vectors[j];
             state->level = 1;
@@ -187,10 +260,11 @@ Cluster* init_clusters(Instance * inst)
         for (int j = 0; j < inst->tables[i]->ncols; j++)
         {
             state = new State;
-            state->table_id = i;
-            state->column_id = j;
+            state->abs_column_id = id;
             state->sum_vector = inst->tables[i]->sum_vectors[j];
             state->sample_size = inst->tables[i]->nrows;
+            state->similarities_vector = new float[inst->total_num_columns];
+            state->reach_probs = new float[inst->total_num_columns];
 
             current = new Cluster;
             current->state = state;
@@ -217,34 +291,48 @@ float** init_dist_matrix(Cluster* active_clusters, int total_num_columns, int em
 
     int size = 2 * total_num_columns - 1;
     float **dist_matrix = new float*[size];
+
+    float similarity;
+
     //Init dist_matrix with zeros
     for(int i = 0; i < size; i++)
         dist_matrix[i] = new float[size];
 
-    for(int i = 0; i < total_num_columns-1; i++)
+    for(int i = 0; i < total_num_columns; i++)
     {
         cluster_j = cluster_i->next;
         for(int j = i+1; j < total_num_columns; j++)
         {
-            dist_matrix[i][j] = 1 - cossine_similarity(cluster_i->state->sum_vector, cluster_j->state->sum_vector, embedding_dim);
+            similarity = cossine_similarity(cluster_i->state->sum_vector, cluster_j->state->sum_vector, embedding_dim);
+            cluster_i->state->similarities_vector[ cluster_j->state->abs_column_id ] = similarity;
+            cluster_j->state->similarities_vector[ cluster_i->state->abs_column_id ] = similarity;
+
+            dist_matrix[i][j] = 1 - similarity;
             dist_matrix[j][i] = dist_matrix[i][j];
             cluster_j = cluster_j->next;
         }
+        cluster_i->state->similarities_vector[ cluster_i->state->abs_column_id ] = 1.0;
+
+        printf("ID: %d\n", cluster_i->state->abs_column_id);
+        for (int z = 0; z < total_num_columns; z++)
+            printf("%.2f ", cluster_i->state->similarities_vector[z]);
+        printf("\n\n");
+
         cluster_i = cluster_i->next;
     }
     
     //TEST!
-    for(int i=0; i<total_num_columns; i++){
-        for(int j =0; j<total_num_columns; j++)
-            printf("%.2f ", dist_matrix[i][j]);
-        printf("\n");
-    }
+    // for(int i=0; i<total_num_columns; i++){
+    //     for(int j =0; j<total_num_columns; j++)
+    //         printf("%.2f ", dist_matrix[i][j]);
+    //     printf("\n");
+    // }
 
     return dist_matrix;
 }
 
 //MERGE THE LAST TWO CLUSTER OF NN CHAIN AND ADD THE NEW CLUSTER INTO UNMERGED CLUSTERS LIST 
-Cluster* merge_clusters(Cluster *stack, float **dist_matrix, int cluster_id, int embedding_dim)
+Cluster* merge_clusters(Cluster *stack, float **dist_matrix, int cluster_id, Instance *inst)
 {
     int id_1 = stack->id;
     int id_2 = stack->is_NN_of->id;
@@ -258,10 +346,26 @@ Cluster* merge_clusters(Cluster *stack, float **dist_matrix, int cluster_id, int
     //CREATE A NEW STATE IN THE ORGANIZATION WHICH WILL BE PARENT OF RNN STATES
     State *new_state = new State;
 
-    new_state->sum_vector = new float[embedding_dim];
-    for (int i = 0; i < embedding_dim; i++)
+    new_state->sum_vector = new float[inst->embedding_dim];
+    for (int i = 0; i < inst->embedding_dim; i++)
         new_state->sum_vector[i] = state_1->sum_vector[i] + state_2->sum_vector[i];
 
+    int table_id;
+    int col_id;
+    float *sum_vector_i;
+    new_state->similarities_vector = new float[inst->total_num_columns];    
+
+    for (int i = 0; i < inst->total_num_columns; i++)
+    {
+        table_id = inst->map[i][0];
+        col_id = inst->map[i][1];
+        sum_vector_i = inst->tables[table_id]->sum_vectors[col_id];
+        new_state->similarities_vector[i] = cossine_similarity(new_state->sum_vector, sum_vector_i, inst->embedding_dim);
+        
+    }
+
+    new_state->abs_column_id = -cluster_id; //NEGATIVE ID: A NON-LEAF STATE
+    new_state->reach_probs = new float[inst->total_num_columns];
     new_state->sample_size = state_1->sample_size + state_2->sample_size;
     new_state->children.push_back(state_1);
     new_state->children.push_back(state_2);
@@ -273,6 +377,7 @@ Cluster* merge_clusters(Cluster *stack, float **dist_matrix, int cluster_id, int
     Cluster *new_cluster = new Cluster;
     new_cluster->state = new_state;
     new_cluster->id = cluster_id;
+    new_cluster->cardinality = card_1 + card_2;
 
     //UPDATE DISTANCES MATRIX
     for (int i = 0; i < cluster_id; i++)
@@ -282,11 +387,11 @@ Cluster* merge_clusters(Cluster *stack, float **dist_matrix, int cluster_id, int
     }
 
     //TEST!
-    for(int i=0; i <= cluster_id; i++){
-        for(int j =0; j <= cluster_id; j++)
-            printf("%.2f ", dist_matrix[i][j]);
-        printf("\n");
-    }
+    // for(int i=0; i <= cluster_id; i++){
+    //     for(int j =0; j <= cluster_id; j++)
+    //         printf("%.2f ", dist_matrix[i][j]);
+    //     printf("\n");
+    // }
 
     return new_cluster;
 }
@@ -317,7 +422,8 @@ Organization generate_organization_by_clustering(Instance * inst, float gamma)
     //WHILE THERE ARE CLUSTERS TO MERGE 
     while(stack != NULL)
     {
-        printf("%d ", stack->id);
+        // printf("Iteration\n\n");
+        // printf("%d ", stack->id);
         prev_nn = NULL;
         nn = active_clusters;
         previous = active_clusters;
@@ -343,9 +449,9 @@ Organization generate_organization_by_clustering(Instance * inst, float gamma)
         }
         if( diff_dists < 0 || ( diff_dists == 0 && stack->is_NN_of->id < nn->id ) )
         {
-            printf("%d\n", stack->is_NN_of->id);
+            // printf("%d\n", stack->is_NN_of->id);
             //MERGE THE RNN INTO A NEW CLUSTER
-            new_cluster = merge_clusters(stack, dist_matrix, cluster_id, org.embedding_dim);
+            new_cluster = merge_clusters(stack, dist_matrix, cluster_id, inst);
             //ADD THE NEW CLUSTER INTO UNMERGED CLUSTER LIST
             new_cluster->next = active_clusters;
             active_clusters = new_cluster;
@@ -365,28 +471,36 @@ Organization generate_organization_by_clustering(Instance * inst, float gamma)
                 prev_nn->next = nn->next;
             
             //ADD NN TO THE NN CHAIN
-            printf("%d\n", nn->id);
+            // printf("%d\n", nn->id);
             nn->is_NN_of = stack;
             stack = nn;
 
             if( active_clusters == NULL ) {
-                active_clusters = merge_clusters(stack, dist_matrix, cluster_id, org.embedding_dim);
+                active_clusters = merge_clusters(stack, dist_matrix, cluster_id, inst);
                 cluster_id++;
                 stack = stack->is_NN_of->is_NN_of;
             }
         }
         //Test!
-        printf("%d\n", stack->id);
-        current = active_clusters;
-        while(current != NULL){
-            printf("%d ", current->id);
-            current = current->next;
-        }
-        printf("\n\n");
+        current = stack;
+        // printf("Stack: ");
+        // while(current != NULL){
+        //     printf("%d ", current->id);
+        //     current = current->is_NN_of;
+        // }
+        // printf("\n");
+
+        // current = active_clusters;
+        // printf("Active Clusters: ");
+        // while(current != NULL){
+        //     printf("%d ", current->id);
+        //     current = current->next;
+        // }
+        // printf("\n\n");
     }
 
     org.root = active_clusters->state;
-    //org.update_all_reach_probs(inst);
+    org.compute_all_reach_probs(inst);
     return org;
 }
 
@@ -414,6 +528,19 @@ Instance read_instance()
             table->sum_vectors[j] = new float[instance.embedding_dim];
             for (int d = 0; d < instance.embedding_dim; d++)
                 scanf("%f", &table->sum_vectors[j][d]);
+        }
+    }
+
+    //GENERATE MAP ABSOLUTE ID -> RELATIVE ID
+    instance.map = new int*[instance.total_num_columns];
+    int count = 0;
+    for (int i = 0; i < instance.num_tables; i++)
+    {
+        for (int j = 0; j < instance.tables[i]->ncols; j++){
+            instance.map[count] = new int[2];
+            instance.map[count][0] = i;
+            instance.map[count][1] = j;
+            count++;
         }
     }
 
