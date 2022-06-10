0d51f778ce3000d55ded1891e56c660fc0bb2e34
danilofernandes45 <danilofernandes45@gmail.com>
2022-06-10 12:38:06 -0300

    Stats

---
+++ RVND/source/utils.hpp
@@ -0,0 +1,10 @@
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+float cossine_similarity(float *vector_1, float *vector_2, int dim);
+
+#endif
