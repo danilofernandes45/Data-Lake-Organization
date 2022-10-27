0ad1720c9160a236d59f0eae9ae07fff7f344a32
danilofernandes45 <danilofernandes45@gmail.com>
2022-10-27 08:11:47 -0300

    More Data

--- Nargesian/perform_evaluation.sh
+++ Nargesian/perform_evaluation.sh
@@ -2,11 +2,11 @@
 function eval() {
 	for i in $(seq 1 15)
 	do
-		./local_search --gamma 25 --time 300 --Kp 79 --eps 0.019 -i ../Data/DataLakes/Socrata/$1/topic_vectors-$1-$2.txt
+		./local_search --gamma 25 --time $2 --Kp 79 --eps 0.019 -i ../Data/DataLakes/Socrata/$1/topic_vectors-$1-$3.txt
 	done
 }
 
-for i in $(seq 1 10)
+for i in $(seq 1 5)
 do
-	eval $1 $i > ../Data/Performance/Socrata/Nargesian/$1/perform-$1-$i.csv
+	eval $1 $2 $i > ../Data/Performance/Socrata/Nargesian/$1/perform-$1-$i.csv
 done
