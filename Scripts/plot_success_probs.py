493bba3e31b3099574731b82b37c0c57e68514e2
danilofernandes45 <danilofernandes45@gmail.com>
2022-11-18 20:47:17 -0300

    Success Probs Data

--- Scripts/plot_success_probs.py
+++ Scripts/plot_success_probs.py
@@ -3,33 +3,83 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 
-plt.rcParams.update({'font.size': 32})
+plt.rcParams.update({'font.size': 35, 'lines.markersize': 15, 'lines.linewidth': 3})
 
-def plotSuccessProbs(data):
+def plotSuccessProbs(data, size):
     success_probs = pd.DataFrame()
+    ids = np.arange(1, size + 1)
+    marker_pos = np.arange(0, size, size // 10) + (size // 10)    
+    line_type = [':s', '-.P' ,'--o', '-^']
+    markers = ['s', 'P' ,'o', '^']
 
     for i in range(data.shape[0]):
         row = data.iloc[i]
         probs = np.array(row['SuccessProbs'].split(' '), dtype = 'float')
-        probs = pd.DataFrame(probs, columns = ['Success Probability'])
-        probs['Desc'] = row['Approach']
-        probs['Table ID'] = np.arange(1, probs.shape[0] + 1)
-        success_probs = pd.concat([success_probs, probs])
+        plt.plot(ids, probs, line_type[i], markersize = 0.1)
+        plt.scatter(marker_pos, probs[marker_pos-1], marker = markers[i])
 
-    success_probs = success_probs.reset_index(drop = True)
-
-    g = sns.lineplot(data=success_probs, x = 'Table ID', y = 'Success Probability', hue = 'Desc')
-    g.legend_.set_title(None)
+    plt.legend(title=None, loc='upper left', labels=data['Approach'], markerscale = 150)
+    plt.xlabel("Table ID")
+    plt.ylabel("Success Probability")
+    plt.grid()
     plt.show()
 
-data = pd.read_csv('../Data/Performance/Socrata/SuccessProbs/success-100-7.csv', sep = '|')
-plotSuccessProbs(data)
+def find_best_plot(size):
+    best_mean_diff = [0, 0]
+    best_id = [-1, -1]
 
-data = pd.read_csv('../Data/Performance/Socrata/SuccessProbs/success-300-1.csv', sep = '|')
-plotSuccessProbs(data)
+    for i in range(10):
+        try:
+            data = pd.read_csv('../Data/Performance/Socrata/SuccessProbs/success-'+str(size)+'-'+str(i)+'.csv', sep = '|')
+            probs_narge = np.array(data.iloc[2]['SuccessProbs'].split(' '), dtype = 'float')
+            probs_sa = np.array(data.iloc[3]['SuccessProbs'].split(' '), dtype = 'float')
+            mean_diff = np.sum(probs_sa - probs_narge) / probs_sa.shape[0]
 
-data = pd.read_csv('../Data/Performance/Socrata/SuccessProbs/success-300-6.csv', sep = '|')
-plotSuccessProbs(data)
+            if mean_diff > best_mean_diff[0] :
+                best_mean_diff[1] = best_mean_diff[0]
+                best_mean_diff[0] = mean_diff
+                best_id[1] = best_id[0]
+                best_id[0] = i
+            elif mean_diff > best_mean_diff[1] :
+                best_mean_diff[1] = mean_diff
+                best_id[1] = i
 
-data = pd.read_csv('../Data/Performance/Socrata/SuccessProbs/success-500-6.csv', sep = '|')
-plotSuccessProbs(data)
+        except:
+            pass
+
+    return best_id, best_mean_diff
+
+
+best_id, best_mean_diff = find_best_plot(100)
+
+print("Best mean diffs")
+print(best_mean_diff)
+print("Best IDs")
+print(best_id)
+
+data = pd.read_csv('../Data/Performance/Socrata/SuccessProbs/success-100-'+str(best_id[0])+'.csv', sep = '|')
+plotSuccessProbs(data, 100)
+data = pd.read_csv('../Data/Performance/Socrata/SuccessProbs/success-100-'+str(best_id[1])+'.csv', sep = '|')
+plotSuccessProbs(data, 100)
+
+best_id, best_mean_diff = find_best_plot(300)
+
+print("Best mean diffs")
+print(best_mean_diff)
+print("Best IDs")
+print(best_id)
+
+# data = pd.read_csv('../Data/Performance/Socrata/SuccessProbs/success-100-7.csv', sep = '|')
+# plotSuccessProbs(data, 100)
+
+# data = pd.read_csv('../Data/Performance/Socrata/SuccessProbs/success-300-1.csv', sep = '|')
+# plotSuccessProbs(data, 300)
+
+# data = pd.read_csv('../Data/Performance/Socrata/SuccessProbs/success-300-2.csv', sep = '|')
+# plotSuccessProbs(data, 300)
+
+# data = pd.read_csv('../Data/Performance/Socrata/SuccessProbs/success-500-6.csv', sep = '|')
+# plotSuccessProbs(data, 500)
+
+# data = pd.read_csv('../Data/Performance/Socrata/SuccessProbs/success-500-10.csv', sep = '|')
+# plotSuccessProbs(data, 500)
--- Scripts/tttplots.py
+++ Scripts/tttplots.py
@@ -4,38 +4,48 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from statsmodels.distributions.empirical_distribution import ECDF
 
-plt.rcParams.update({'font.size': 32})
+plt.rcParams.update({'font.size': 35, 'lines.markersize': 15, 'lines.linewidth': 3})
 
 def plotECDF(data1, data2):
-    probs = pd.DataFrame()
     ecdf = ECDF(data1[0])
-    ecdf = pd.DataFrame(np.array([ecdf.x, ecdf.y]).T)
-    ecdf = ecdf.groupby([0]).max().reset_index()
-    ecdf.iloc[0, 0] = 0
-    ecdf['approach'] = 'Nargesian'
-    probs = pd.concat([probs, ecdf])
+    data1 = pd.DataFrame(np.array([ecdf.x, ecdf.y]).T)
+    data1 = data1.groupby([0]).max().reset_index()
+    data1.iloc[0, 0] = 0
 
     ecdf = ECDF(data2[0])
-    ecdf = pd.DataFrame(np.array([ecdf.x, ecdf.y]).T)
-    ecdf = ecdf.groupby([0]).max().reset_index()
-    ecdf.iloc[0, 0] = 0
-    ecdf['approach'] = 'SA'
-    probs = pd.concat([probs, ecdf])
+    data2 = pd.DataFrame(np.array([ecdf.x, ecdf.y]).T)
+    data2 = data2.groupby([0]).max().reset_index()
+    data2.iloc[0, 0] = 0
 
-    probs = probs.reset_index(drop = True)
-    probs.columns = ['x', 'y', 'approach']
+    red = sns.color_palette()[3]
+    green = sns.color_palette()[2]
 
-    plt.plot(ecdf[0], ecdf[1], 'g--')
-    plt.plot(ecdf[0], ecdf[1]-0.1, 'r-')
+    plt.plot(data1[0], data1[1], '--o', color = green)
+    plt.plot(data2[0], data2[1], '-^', color = red)
+
     # sns.color_palette(["green", "red"])
     # g = sns.lineplot(data=probs, x = 'x', y = 'y', hue = 'approach', legend = False)
-    plt.legend(title=None, loc='lower right', labels=['Nargesian', 'SA'])
+    plt.legend(title=None, loc='upper left', labels=['Nargesian', 'SA'])
+
+    plt.xlabel("Time to target (seconds)")
+    plt.ylabel("Cumulative probability")
+    plt.grid()
+
     plt.show()
 
 
-data1 = pd.read_csv("../Data/Performance/Socrata/tttplot/nargesian-tttplot-100-3.txt", header = None)
-data2 = pd.read_csv("../Data/Performance/Socrata/tttplot/sa-tttplot-100-3.txt", header = None)
+# data1 = pd.read_csv("../Data/Performance/Socrata/tttplot/nargesian-tttplot-100-3.txt", header = None)
+# data2 = pd.read_csv("../Data/Performance/Socrata/tttplot/sa-tttplot-100-3.txt", header = None)
+# plotECDF(data1, data2)
+
+data1 = pd.read_csv("../Data/Performance/Socrata/tttplot/nargesian-tttplot-100-5.txt", header = None)
+data2 = pd.read_csv("../Data/Performance/Socrata/tttplot/sa-tttplot-100-5.txt", header = None)
 plotECDF(data1, data2)
-# ecdf = ECDF(data[0])
-# plt.plot(ecdf.x, ecdf.y, '.:g') # --, -.
-# plt.show()
+
+data1 = pd.read_csv("../Data/Performance/Socrata/tttplot/nargesian-tttplot-300-6.txt", header = None)
+data2 = pd.read_csv("../Data/Performance/Socrata/tttplot/sa-tttplot-300-6.txt", header = None)
+plotECDF(data1, data2)
+
+data1 = pd.read_csv("../Data/Performance/Socrata/tttplot/nargesian-tttplot-500-6.txt", header = None)
+data2 = pd.read_csv("../Data/Performance/Socrata/tttplot/sa-tttplot-500-6.txt", header = None)
+plotECDF(data1, data2)
